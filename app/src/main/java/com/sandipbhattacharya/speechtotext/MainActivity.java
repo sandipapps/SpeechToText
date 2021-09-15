f4f9e94a98ab57c0b6ddb56be94c145597210f94
Sandip Bhattacharya <sandip.bhattacharya16@gmail.com>
2021-09-15 18:43:18 +0530

    Source code for YouTube tutorial "How to Build a Speech to Text App in Android Studio"

---
+++ app/src/main/java/com/sandipbhattacharya/speechtotext/MainActivity.java
@@ -0,0 +1,58 @@
+package com.sandipbhattacharya.speechtotext;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected static final int RESULT_SPEECH = 1;
+    private ImageButton btnSpeak;
+    private TextView tvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvText = findViewById(R.id.tvText);
+        btnSpeak = findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    tvText.setText("");
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Your device doesn't support Speech to Text", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_SPEECH:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    tvText.setText(text.get(0));
+                }
+                break;
+        }
+    }
+}
